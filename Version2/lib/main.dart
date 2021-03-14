eb6dfe34c09beee2b72b0df93d319f8301c9255a
Nikeshh <nikeshhbaskaran01@gmail.com>
2021-03-14 18:55:02 +0530

    Initialized different versions

---
+++ Version2/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/constants.dart';
+import 'package:portfolio/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nikeshh V - Portfolio',
+      theme: ThemeData(
+        inputDecorationTheme: kDefaultInputDecorationTheme,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
