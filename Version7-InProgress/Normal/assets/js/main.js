05ade96f1885b9f9f16c9d1e9b27970a55fc5017
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2021-12-18 13:18:07 -0500

    NPW-45 Hero section changes

--- Version7-InProgress/Normal/assets/js/main.js
+++ Version7-InProgress/Normal/assets/js/main.js
@@ -42,4 +42,20 @@
             this.nextElementSibling.classList.toggle('dropdown-active')
         }
     }, true)
+
+    /**
+     * Animation on scroll
+     */
+    function aos_init() {
+        AOS.init({
+            duration: 1000,
+            easing: "ease-in-out",
+            once: true,
+            mirror: false
+        });
+    }
+    window.addEventListener('load', () => {
+        aos_init();
+    });
+
 })();
