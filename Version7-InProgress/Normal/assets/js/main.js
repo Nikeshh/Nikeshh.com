108d2c179995d9fbfad5d7c4577f39a1aeafa021
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2021-12-24 08:38:35 -0500

    NPW-50 Portfolio section isotope and filter changes

--- Version7-InProgress/Normal/assets/js/main.js
+++ Version7-InProgress/Normal/assets/js/main.js
@@ -184,4 +184,31 @@
         selector: '.portfolio-lightbox'
     });
 
+    /**
+     * Portfolo isotope and filter
+     */
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if(portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item',
+                layoutMode: 'fitRows'
+            });
+            let portfolioFilters = select('#portfolio-filters li', true);
+
+            on('click', '#portfolio-filters li', function(e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function(el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                aos_init();
+            }, true);
+        }
+    });
+
 })();
