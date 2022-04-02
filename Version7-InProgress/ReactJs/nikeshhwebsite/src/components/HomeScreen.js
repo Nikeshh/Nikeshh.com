c883ac0f1da10ee9d2ccd3fa213a9d1237ee1ad7
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-02 12:56:45 -0400

    NPW-72 Home Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/HomeScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/HomeScreen.js
@@ -19,6 +19,33 @@ const HomeScreen = () => {
                     <li>Profile</li>
                 </ul>
             </div>
+            <div>
+                <p>Hi, symbol! Welcome to my site!</p>
+                <h2>I'm Nikeshh Vijayabaskaran</h2>
+                <h2>Full-Stack Developer, Mentor, Proud Son, Hardcore coder and Creative most approachable teammate</h2>
+                <p>I design, code and rebuild the software from scratch that can scale 100x, and I love what I do every day.</p>
+                <p>I’m available for freelance opportunities as well.</p>
+                <h4>Email: nikeshhbaskaran01@gmail.com</h4>
+                <h4>Phone number: +1 416 834 5350</h4>
+            </div>
+            <div>
+                <ul>
+                    <li>LinkedIn</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Discord</li>
+                    <li>Slack</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>HIRE ME</li>
+                    <li>DOWNLOAD CV</li>
+                    <li>DOWNLOAD NEWSLETTER</li>
+                </ul>
+            </div>
         </>
     );
 }
