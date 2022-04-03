694e24b4c85cb871fa2afe4c0db75c50d44c828b
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-03 12:50:00 -0400

    NPW-75 Certifications and Awards Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/CertificationsScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/CertificationsScreen.js
@@ -7,7 +7,130 @@ const CertificationsScreen = () => {
         <>
             <Header />
             <main>
-                
+                <div className="row justify-content-center">
+                    <h1>CERTIFICATIONS</h1>
+                    <div className="row">
+                        <button>WEB DEVELOPMENT</button>
+                        <button>APP DEVELOPMENT</button>
+                        <button>AI &amp; ML</button>
+                    </div>
+                </div>
+                <section>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <h1>1</h1>
+                            <img src={"/logo192.png"} alt="logo" />
+                        </div>
+                        <div className="col-lg-8">
+                            <h1>Certificate Title</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                                eu tortor euismod, quis vehicula velit dictum. 
+                                Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                                ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                                Sed a ex a ante ultricies fermentum ac eget nibh. 
+                                Maecenas nec odio volutpat, scelerisque leo non, 
+                                efficitur mauris.
+                            </p>
+                            <p>Certificate link</p>
+                            <p>Certificate credential id</p>
+                            <div className="row">
+                                <button>MORE</button>
+                                <button>PROJECTS</button>
+                                <button>TUTORIALS</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <h1>2</h1>
+                            <img src={"/logo192.png"} alt="logo" />
+                        </div>
+                        <div className="col-lg-8">
+                            <h1>Certificate Title</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                                eu tortor euismod, quis vehicula velit dictum. 
+                                Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                                ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                                Sed a ex a ante ultricies fermentum ac eget nibh. 
+                                Maecenas nec odio volutpat, scelerisque leo non, 
+                                efficitur mauris.
+                            </p>
+                            <p>Certificate link</p>
+                            <p>Certificate credential id</p>
+                            <div className="row">
+                                <button>MORE</button>
+                                <button>PROJECTS</button>
+                                <button>TUTORIALS</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="row justify-content-center">
+                    <h1>AWARDS</h1>
+                    <div className="row">
+                        <button>WEB DEVELOPMENT</button>
+                        <button>APP DEVELOPMENT</button>
+                        <button>AI &amp; ML</button>
+                    </div>
+                </div>
+                <section>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <h1>1</h1>
+                            <img src={"/logo192.png"} alt="logo" />
+                        </div>
+                        <div className="col-lg-8">
+                            <h1>Award Title</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                                eu tortor euismod, quis vehicula velit dictum. 
+                                Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                                ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                                Sed a ex a ante ultricies fermentum ac eget nibh. 
+                                Maecenas nec odio volutpat, scelerisque leo non, 
+                                efficitur mauris.
+                            </p>
+                            <p>Award link</p>
+                            <p>Award credential id</p>
+                            <div className="row">
+                                <button>MORE</button>
+                                <button>PROJECTS</button>
+                                <button>TUTORIALS</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <h1>2</h1>
+                            <img src={"/logo192.png"} alt="logo" />
+                        </div>
+                        <div className="col-lg-8">
+                            <h1>Certificate Title</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                                eu tortor euismod, quis vehicula velit dictum. 
+                                Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                                ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                                Sed a ex a ante ultricies fermentum ac eget nibh. 
+                                Maecenas nec odio volutpat, scelerisque leo non, 
+                                efficitur mauris.
+                            </p>
+                            <p>Award link</p>
+                            <p>Award credential id</p>
+                            <div className="row">
+                                <button>MORE</button>
+                                <button>PROJECTS</button>
+                                <button>TUTORIALS</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
             <Footer />
         </>
