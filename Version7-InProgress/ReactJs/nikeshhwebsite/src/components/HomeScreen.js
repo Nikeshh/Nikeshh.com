38753bdafdf537fd5d7a31198b191c4f1ed72ae0
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-02 21:47:50 -0400

    NPW-72 Home Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/HomeScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/HomeScreen.js
@@ -215,6 +215,38 @@ const HomeScreen = () => {
                         </div>
                     </div>
                 </section>
+                <div id="footer" className="d-flex">
+                    <img src={"/logo192.png"} alt="logo" />
+                    <h1>Nikeshh V</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Skills</li>
+                        <li>Works</li>
+                        <li>Testimonials</li>
+                        <li>Mentorship</li>
+                        <li>Blog</li>
+                        <li>Support</li>
+                        <li>SAY HELLO / HIRE ME</li>
+                        <li>Profile</li>
+                    </ul>
+                </div>
+                <hr />
+                <div className="d-flex">
+                    <p>TERMS OF USE</p>
+                    <i className="fa fa-info"></i>
+                    <p>INFO ABOUT WEBSITE</p>
+                    <ul>
+                        <li>LinkedIn</li>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Discord</li>
+                        <li>Slack</li>
+                        <li>Email</li>
+                    </ul>
+                    <p>COPYRIGHT 2022. MADE WITH LOVE BY NIKESHH VIJAYABASKARAN.</p>
+                </div>
             </main>
         </>
     );
