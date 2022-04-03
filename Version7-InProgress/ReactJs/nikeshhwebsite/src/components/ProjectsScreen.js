8d31c57c24e7b27d08fd6db01e7ff4ca42246368
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-03 13:04:43 -0400

    NPW-76 Projects and Works Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ProjectsScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ProjectsScreen.js
@@ -7,7 +7,50 @@ const ProjectsScreen = () => {
         <>
             <Header />
             <main>
+                <section>
+                    <div>
+                        <p>PROJECTS</p>
+                        <h3>What have I done?</h3>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <h4>All Projects v</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                            <h4>Category Title</h4>
+                        </div>
+                        <div className="col-lg-8">
+                            <table>
+                               <thead>
+                                    <tr>
+                                        <th>PROJECT NAME</th>
+                                        <th>TAGS</th>
+                                    </tr>
+                               </thead>
+                               <tbody>
+                                    <tr>
+                                        <td>
+                                            
+                                        </td>
+                                        <td>
 
+                                        </td>
+                                        <td>v</td>
+                                    </tr>
+                               </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
             </main>
             <Footer />
         </>
