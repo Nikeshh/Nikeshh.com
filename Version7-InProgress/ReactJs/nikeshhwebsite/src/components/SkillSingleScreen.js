ad3717da826cea212b8458f94aef70d2e16ede25
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-03 12:22:03 -0400

    NPW-74 Skill Single, Linked Projects and Linked Blogs Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/SkillSingleScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/SkillSingleScreen.js
@@ -7,7 +7,33 @@ const SkillSingleScreen = () => {
         <>
             <Header />
             <main>
-                
+                <section>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <img src={"/logo192.png"} alt="logo" />
+                        </div>
+                        <div className="col-lg-8">
+                            <h1>Lorem Ipsum</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                                eu tortor euismod, quis vehicula velit dictum. 
+                                Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                                ornare lectus, ut gravida eros velit a libero.
+                            </p>
+                            <h3>
+                                My skill expertise : 
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </section>
             </main>
             <Footer />
         </>
