f2b11181a91fac914ddef8037d9580759d5a2783
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-03 12:26:19 -0400

    NPW-74 Skill Single, Linked Projects and Linked Blogs Screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/SkillSingleScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/SkillSingleScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Projects from "./home/Projects";
+import ReadMyWritings from "./home/ReadMyWritings";
 import Footer from "./util/Footer";
 import Header from "./util/Header";
 
@@ -32,6 +34,35 @@ const SkillSingleScreen = () => {
                                 </span>
                             </h3>
                         </div>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <p>PROJECTS / WORKS</p>
+                        <h3>What have I done?</h3>
+                    </div>
+                    <div className="row">
+                        <p>THE BELOW PROJECTS IS ONLY BASED ON THE ABOVE SKILL. TO SEE OTHER PROJECTS</p>
+                        <button>CLICK HERE</button>
+                    </div>
+                    <div className="row justify-content-between">
+                        <Projects threeColumn={true} />
+                    </div>
+                    <div className="row justify-content-between">
+                        <Projects threeColumn={true} />
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <p>BLOG</p>
+                        <h3>READ SOME OF MY WRITINGS</h3>
+                    </div>
+                    <div className="row">
+                        <p>THE BELOW PROJECTS IS ONLY BASED ON THE ABOVE SKILL. TO SEE OTHER PROJECTS</p>
+                        <button>CLICK HERE</button>
+                    </div>
+                    <div className="row justify-content-between">
+                        <ReadMyWritings />
                     </div>
                 </section>
             </main>
--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Projects.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Projects.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Projects = () => {
+const Projects = (props) => {
+
+    const threeColumn = props.threeColumn;
+
     return (
         <>
             <div className="col-lg-3">
@@ -27,9 +30,24 @@ const Projects = () => {
                     </p>
                 </div>
             </div>
-            <div className="col-lg-3">
-                <button>More</button>
-            </div>
+            {threeColumn ? (
+                <div className="col-lg-3">
+                    <div className="card">
+                        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                            <i className="fa fa-heart"></i>
+                        </div>
+                        <img src={"/logo192.png"} alt="skill" />
+                        <h5>Project Title</h5>
+                        <p>
+                            Project description
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                <div className="col-lg-3">
+                    <button>More</button>
+                </div>
+            )}
         </>
     );
 };
