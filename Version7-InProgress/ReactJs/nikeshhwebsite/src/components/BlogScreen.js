c978264583fff7b2cd5051cadf367684a0fe5ed1
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-04 14:03:58 -0400

    NPW-77 Blog screen changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/BlogScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/BlogScreen.js
@@ -6,6 +6,19 @@ const BlogScreen = () => {
     return (
         <>
             <Header />
+            <div className="d-flex justify-content-between">
+                <ul className="d-flex">
+                    <li>HOME</li>
+                    <li>UI/UX DESIGN</li>
+                    <li>WEB DEVELOPMENT</li>
+                    <li>TYPOGRAPHY</li>
+                    <li>FREELANCE BUSINESS</li>
+                    <li>MORE</li>
+                </ul>
+                <div>
+                    <input type="text" value="Search..."></input>
+                </div>
+            </div>
             <Footer />
         </>
     );
