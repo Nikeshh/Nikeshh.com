7458a7ed570f6bde4b26b6b211b4035f62fbf2d8
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-04-28 23:34:51 -0400

    Blog changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/BlogSingleScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/BlogSingleScreen.js
@@ -21,24 +21,37 @@ const BlogSingleScreen = () => {
                 <span> &#8592;</span> <span>Go Back</span>
             </Link>
             {blog ? (
-                <div className="blog-wrap">
-                    <header>
-                        <p className="blog-date">Published {blog.createdAt}</p>
-                        <h1>{blog.title}</h1>
-                        <div className="blog-subCategory">
-                            {blog.subCategory.map((category, i) => (
-                                <div key={i}>
-                                    <p className="chip">{category}</p>
+                <>
+                    <section className="blog">
+                        <div className="blog-wrap">
+                            <header>
+                                <p className="blog-date">Published {blog.createdAt}</p>
+                                <h1>{blog.title}</h1>
+                                <div className="blog-subCategory">
+                                    {blog.subCategory.map((category, i) => (
+                                        <div key={i}>
+                                            <p className="chip">{category}</p>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
+                            </header>
+                            <img src={blog.cover} alt='cover' />
+                            <p className="blog-desc">{blog.description}</p>
                         </div>
-                    </header>
-                    <img src={blog.cover} alt='cover' />
-                    <p className="blog-desc">{blog.description}</p>
-                </div>
+                        <div className="blog-footer">
+
+                        </div>
+                    </section>
+                    <section className="slider">
+
+                    </section>
+                </>
             ) : (
                 <EmptyList />
             )}
+            <footer id="footer">
+                
+            </footer>
         </>
     );
 }
