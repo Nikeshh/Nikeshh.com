559764b5cd92a2d8e18ecfbe361dd97d0f207103
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-05-18 15:45:40 -0400

    Home page changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ForumScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ForumScreen.js
@@ -1,20 +1,8 @@
 import React from "react";
-import Controls from "./forum/Controls";
-import Header from "./forum/Header";
-import Posts from "./forum/Posts";
-import './forum/style.css';
 
 const ForumScreen = () => {
     return (
-        <>
-            <Header />
-            <main>
-                <div className="container">
-                    <Controls />
-                    <Posts />
-                </div>
-            </main>
-        </>
+        <h1>Forum Screen</h1>
     );
 }
 
--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/util/Header.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/util/Header.js
@@ -2,7 +2,41 @@ import React from "react";
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <>
+            <header>
+                <div className="container header">
+                    <div className="row header__row">
+                        <div className="header__logo">
+                            <img src={"./assets/images/logo.svg"} alt="logo" />
+                            <span>Nikeshh V</span>
+                        </div>
+                        <nav className="header__tabs__nav">
+                            <ul className="header__tabs">
+                                <li><a href="#" className="active">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Skills</a></li>
+                                <li><a href="#">Works</a></li>
+                                <li><a href="#">Testimonials</a></li>
+                                <li><a href="#">Mentorship</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="#" className="header__button">Say Hello / Hire Me</a></li>
+                                <li>
+                                    <span>EN</span>
+                                    <img src={"./assets/images/Circle_arrow_down_font_awesome.svg"} alt="down_arrow" style={{width:"20px", height: "20px"}} />
+                                    <nav className="language__nav">
+                                        <ul className="language">
+                                            <li><a href="#" className="active">EN</a></li>
+                                            <li><a href="#">FR</a></li>
+                                        </ul>
+                                    </nav>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 }
 
