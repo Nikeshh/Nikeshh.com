875042bbdef97e82e937f997ed039f40b51c90be
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-05-25 20:21:59 -0400

    Skills and services changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/SkillsAndServices.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/SkillsAndServices.js
@@ -1,8 +1,48 @@
 import React from "react";
+import "./skillsandservices.css";
+import Skills from "./skillsandservices/Skills";
 
 const SkillsAndServices = () => {
     return (
-        <h1></h1>
+        <div className="skillsandservices">
+            <span className="title">Skills And Services</span>
+            <h3 className="subtitle">What Am I Capable Of?</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                </div>
+                <div className="row" style={{ marginTop: "20px" }}>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                </div>
+                <div className="row" style={{ marginTop: "20px" }}>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                    <div className="col">
+                        <Skills />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
---
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/skillsandservices/Skills.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <>
+            <div className="card" style={{width: "18rem"}}>
+                <img src={"./assets/images/landing_image.png"} className="card-img-top" alt="Skill" />
+                <div className="card-body">
+                    <div className="skills-title-and-ratings">
+                        <h5 className="card-text">Web Services</h5>
+                        <div className="ratings">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star-half-stroke"></i>
+                        </div>
+                    </div>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className="card-body">
+                    <a href="#" className="card-link">Contact Me <i class="fa-solid fa-address-book"></i></a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Skills;
