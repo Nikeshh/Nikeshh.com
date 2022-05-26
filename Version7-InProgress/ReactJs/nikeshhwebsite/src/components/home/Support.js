de19338fdf204b1017bc760d2f8bb25b1e5d071a
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-05-26 11:22:03 -0400

    Support changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Support.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Support.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./support.css";
+import line from "../../images/line.png";
 
 const Support = () => {
     return (
-        <h1></h1>
+        <div className="support">
+            <span className="title">Support</span>
+            <h3 className="subtitle">Like My Work?</h3>
+            <div className="content">
+                <div className="content_with_line">
+                    <h3>If you like my work.</h3>
+                    <img src={line} alt="Line" className="line" />
+                </div>
+                <h3>You can always support me using the following links.</h3>
+                <img src={"./assets/images/patreon.png"} className="patreon" alt="patreon" />
+                <img src={"./assets/images/buymeacoffee.png"} className="buymeacoffee" alt="buymeacoffee" />
+                <div className="button_with_line">
+                    <button className="btn btn-warning"><span>Know More</span></button>
+                    <img src={line} alt="Line" className="line" />
+                </div>
+            </div>
+        </div>
     );
 }
 
