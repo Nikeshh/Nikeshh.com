82d191571d54395f6ab9b66477bbffad7b584810
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-05-26 11:56:50 -0400

    Newsletter changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Newsletter.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Newsletter.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./newsletter.css";
 
 const Newsletter = () => {
     return (
-        <h1></h1>
+        <div className="newsletter">
+            <div className="row">
+                <div className="col-md-8">
+                    <span className="title">Newsletter</span>
+                    <h3 className="subtitle">I WRITE ON TOPICS LIKE TECHNOLOGY, CODING, PRODUCT DEVELOPMENT ETC</h3>
+                </div>
+                <div className="col-md-4">
+                    <button className="btn btn-warning"><i className="fa-solid fa-download"></i><span>DOWNLOAD NEWSLETTERS</span></button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-4">
+                    <h3>Newsletter Signup</h3>
+                </div>
+                <div className="col-md-8">
+                    <h3>A input box will come here</h3>
+                </div>
+            </div>
+        </div>
     );
 }
 
