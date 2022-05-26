67a6d280a421ce67beecd5c4221efd779c73992a
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-05-25 22:14:59 -0400

    Testimonials changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Testimonials.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Testimonials.js
@@ -4,6 +4,8 @@ import "./testimonails.css";
 const Testimonials = () => {
     return (
         <div className="testimonails">
+            <span className="title">Testimonials</span>
+            <h3 className="subtitle">See What Others Said About Me</h3>
             <h1>Leaving this portion for testimonials</h1>
         </div>
     );
