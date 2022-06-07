3382d037f1e27448ed2a001f0d0a9e86bb06a691
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-07 11:52:44 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/blog/Blog.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/blog/Blog.js
@@ -46,6 +46,170 @@ const Blog = () => {
                             <p className="right">TYPOGRAPHY</p>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="main-feature">
+                                <div className="card" style={{ width: '22rem' }}>
+                                    <img className="card-img-top" src={"./assets/images/featured.png"} alt="Main Featued Image" />
+                                    <div className="card-body">
+                                        <div className="metadata">
+                                            <div className="post-date">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                <span>20 MAR 2022</span>
+                                            </div>
+                                            <div className="author">
+                                                <i class="fa fa-user" aria-hidden="true"></i>
+                                                <span>Nikeshh</span>
+                                            </div>
+                                            <div className="post-read-time">
+                                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                                <span>5 min read</span>
+                                            </div>
+                                        </div>
+                                        <h4>Google Just Showed Us the Future Of Gaming</h4>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur 
+                                            adipiscing elit. Etiam at ultricies dui…
+                                        </p>
+                                        <p>Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="row sub-feature">
+                                <div className="col-lg-4">
+                                    <img src={"./assets/images/featured.png"} alt="Sub Featued Image" width="300px" />
+                                </div>
+                                <div className="col-lg-8">
+                                    <h3>How a Limitless Internet Binds Us</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur 
+                                        adipiscing elit. Etiam at ultricies dui, eu mattis 
+                                        nisl. Etiam placerat orci eu tortor euismod, quis 
+                                        vehicula velit dictum. 
+                                    </p>
+                                    <h5>Eric Ravensoft in One Zero</h5>
+                                    <p>MAR 20 . 5 MIN READ</p>
+                                </div>
+                            </div>
+                            <div className="row sub-feature">
+                                <div className="col-lg-4">
+                                    <img src={"./assets/images/featured.png"} alt="Sub Featued Image" width="300px" />
+                                </div>
+                                <div className="col-lg-8">
+                                    <h3>How a Limitless Internet Binds Us</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur 
+                                        adipiscing elit. Etiam at ultricies dui, eu mattis 
+                                        nisl. Etiam placerat orci eu tortor euismod, quis 
+                                        vehicula velit dictum. 
+                                    </p>
+                                    <h5>Eric Ravensoft in One Zero</h5>
+                                    <p>MAR 20 . 5 MIN READ</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row" style={{ marginTop: '20px' }}>
+                        <div className="col-lg-4">
+                            <div className="main-feature">
+                                <div className="card" style={{ width: '22rem' }}>
+                                    <img className="card-img-top" src={"./assets/images/featured.png"} alt="Main Featued Image" />
+                                    <div className="card-body">
+                                        <div className="metadata">
+                                            <div className="post-date">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                <span>20 MAR 2022</span>
+                                            </div>
+                                            <div className="author">
+                                                <i class="fa fa-user" aria-hidden="true"></i>
+                                                <span>Nikeshh</span>
+                                            </div>
+                                            <div className="post-read-time">
+                                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                                <span>5 min read</span>
+                                            </div>
+                                        </div>
+                                        <h4>Google Just Showed Us the Future Of Gaming</h4>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur 
+                                            adipiscing elit. Etiam at ultricies dui…
+                                        </p>
+                                        <p>Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="main-feature">
+                                <div className="card" style={{ width: '22rem' }}>
+                                    <img className="card-img-top" src={"./assets/images/featured.png"} alt="Main Featued Image" />
+                                    <div className="card-body">
+                                        <div className="metadata">
+                                            <div className="post-date">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                <span>20 MAR 2022</span>
+                                            </div>
+                                            <div className="author">
+                                                <i class="fa fa-user" aria-hidden="true"></i>
+                                                <span>Nikeshh</span>
+                                            </div>
+                                            <div className="post-read-time">
+                                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                                <span>5 min read</span>
+                                            </div>
+                                        </div>
+                                        <h4>Google Just Showed Us the Future Of Gaming</h4>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur 
+                                            adipiscing elit. Etiam at ultricies dui…
+                                        </p>
+                                        <p>Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="main-feature">
+                                <div className="card" style={{ width: '22rem' }}>
+                                    <img className="card-img-top" src={"./assets/images/featured.png"} alt="Main Featued Image" />
+                                    <div className="card-body">
+                                        <div className="metadata">
+                                            <div className="post-date">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                <span>20 MAR 2022</span>
+                                            </div>
+                                            <div className="author">
+                                                <i class="fa fa-user" aria-hidden="true"></i>
+                                                <span>Nikeshh</span>
+                                            </div>
+                                            <div className="post-read-time">
+                                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                                <span>5 min read</span>
+                                            </div>
+                                        </div>
+                                        <h4>Google Just Showed Us the Future Of Gaming</h4>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur 
+                                            adipiscing elit. Etiam at ultricies dui…
+                                        </p>
+                                        <p>Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="blog-content-latest-posts-and-popular-posts">
+                    <div className="row">
+                        <div className="latest-posts">
+
+                        </div>
+                        <div className="popular-posts">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
