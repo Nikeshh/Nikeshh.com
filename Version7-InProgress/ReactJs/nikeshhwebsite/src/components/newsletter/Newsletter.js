a88e7c2c078c758d167449c7d70c0672b1cbf34f
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-07 23:28:49 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
@@ -6,6 +6,90 @@ const Newsletter = () => {
         <div className="newsletter">
             <span className="title">Newsletter</span>
             <h3 className="subtitle">I Write On Topics Like Technology, Coding, Product Development Etc</h3>
+            <div className="row">
+                <div className="col-lg-4">
+                    <h3>Newsletter Signup</h3>
+                </div>
+                <div className="col-lg-8">
+                    <div style={{ display: 'flex' }}>
+                        <input type="email" id="email_id" name="email_id" placeholder="Your email id" />
+                        <i className="fa fa-arrow-right"></i>
+                    </div>
+                </div>
+            </div>
+            <div className="newsletter_downloads">
+                <h2>Newsletter Downloads</h2>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>01</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <h5>Orgaize the Content Moderators</h5>
+                        <h6>Eric Ravensoft in UI / UX Design</h6>
+                        <p>MAR 20 . 5 MIN READ</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <button>Preview</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>02</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <h5>Orgaize the Content Moderators</h5>
+                        <h6>Eric Ravensoft in UI / UX Design</h6>
+                        <p>MAR 20 . 5 MIN READ</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <button>Preview</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>03</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <h5>Orgaize the Content Moderators</h5>
+                        <h6>Eric Ravensoft in UI / UX Design</h6>
+                        <p>MAR 20 . 5 MIN READ</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <button>Preview</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>04</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <h5>Orgaize the Content Moderators</h5>
+                        <h6>Eric Ravensoft in UI / UX Design</h6>
+                        <p>MAR 20 . 5 MIN READ</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <button>Preview</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <p>05</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <h5>Orgaize the Content Moderators</h5>
+                        <h6>Eric Ravensoft in UI / UX Design</h6>
+                        <p>MAR 20 . 5 MIN READ</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <button>Preview</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
