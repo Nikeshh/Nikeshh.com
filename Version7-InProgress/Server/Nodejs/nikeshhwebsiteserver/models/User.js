2cf119a921dec024baff9b4d77562a1cd26be215
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-08 17:17:04 -0400

    Backend changes

--- Version7-InProgress/Server/Nodejs/nikeshhwebsiteserver/models/User.js
+++ Version7-InProgress/Server/Nodejs/nikeshhwebsiteserver/models/User.js
@@ -27,4 +27,40 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         validate: [validator.isEmail, 'Please provide a valid email']
     },
+    phonenumber: {
+        type: Number,
+        required: [true, 'Please tell us your phone number!'],
+        trim: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [8, 'Must be greater than 8'],
+        select: false
+    },
+    activationLink: {
+        type: String,
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    activated: {
+        type: Boolean,
+        default: true,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    }
 });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
