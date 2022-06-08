eb685cbb3db526ace36c625c45232092eb798b66
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-08 00:34:00 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/certifications/Certifications.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/certifications/Certifications.js
@@ -3,10 +3,136 @@ import "./certifications.css";
 
 const Certifications = () => {
     return (
-        <div className="certifications">
-            <span className="title">Support</span>
-            <h3 className="subtitle">Like my work?</h3>
-        </div>
+        <>
+            <div className="certifications">
+                <span className="title">Certifications</span>
+                <ul>
+                    <li>Web Devlopment</li>
+                    <li>App Development</li>
+                    <li>AI & ML</li>
+                </ul>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={"./assets/images/landing_image.png"} alt="Certification Image" width="500px" />
+                        <div className="certificate_number">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <h3>Certificate Title</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                            eu tortor euismod, quis vehicula velit dictum. 
+                            Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                            ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                            Sed a ex a ante ultricies fermentum ac eget nibh. 
+                            Maecenas nec odio volutpat, scelerisque leo non, 
+                            efficitur mauris.
+                        </p>
+                        <a href="#">Certificate link</a>
+                        <a href="#">Certificate credential id</a>
+                        <div>
+                            <button>More</button>
+                            <button>Projects</button>
+                            <button>Tutorials</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={"./assets/images/landing_image.png"} alt="Certification Image" width="500px" />
+                        <div className="certificate_number">
+                            <p>2</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <h3>Certificate Title</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                            eu tortor euismod, quis vehicula velit dictum. 
+                            Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                            ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                            Sed a ex a ante ultricies fermentum ac eget nibh. 
+                            Maecenas nec odio volutpat, scelerisque leo non, 
+                            efficitur mauris.
+                        </p>
+                        <a href="#">Certificate link</a>
+                        <a href="#">Certificate credential id</a>
+                        <div>
+                            <button>More</button>
+                            <button>Projects</button>
+                            <button>Tutorials</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="awards">
+                <span className="title">Awards</span>
+                <ul>
+                    <li>Web Devlopment</li>
+                    <li>App Development</li>
+                    <li>AI & ML</li>
+                </ul>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={"./assets/images/landing_image.png"} alt="Certification Image" width="500px" />
+                        <div className="award_number">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <h3>Award Title</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                            eu tortor euismod, quis vehicula velit dictum. 
+                            Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                            ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                            Sed a ex a ante ultricies fermentum ac eget nibh. 
+                            Maecenas nec odio volutpat, scelerisque leo non, 
+                            efficitur mauris.
+                        </p>
+                        <a href="#">Award link</a>
+                        <a href="#">Award credential id</a>
+                        <div>
+                            <button>More</button>
+                            <button>Projects</button>
+                            <button>Tutorials</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={"./assets/images/landing_image.png"} alt="Certification Image" width="500px" />
+                        <div className="award_number">
+                            <p>2</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <h3>Award Title</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                            eu tortor euismod, quis vehicula velit dictum. 
+                            Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                            ornare lectus, ut gravida eros velit a libero. In a eros felis. 
+                            Sed a ex a ante ultricies fermentum ac eget nibh. 
+                            Maecenas nec odio volutpat, scelerisque leo non, 
+                            efficitur mauris.
+                        </p>
+                        <a href="#">Award link</a>
+                        <a href="#">Award credential id</a>
+                        <div>
+                            <button>More</button>
+                            <button>Projects</button>
+                            <button>Tutorials</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
