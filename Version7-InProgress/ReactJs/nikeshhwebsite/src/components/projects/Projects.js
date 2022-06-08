d248fe6d1abe9b53a053c55b4416f73147efe033
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-08 00:40:02 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/projects/Projects.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/projects/Projects.js
@@ -3,10 +3,80 @@ import "./projects.css";
 
 const Projects = () => {
     return (
-        <div className="projects">
-            <span className="title">Support</span>
-            <h3 className="subtitle">Like my work?</h3>
-        </div>
+        <>
+            <div className="projects">
+                <span className="title">Projects / Works</span>
+                <h3 className="subtitle">What Have I Done?</h3>
+                <div className="projects_content">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="works">
+                <span className="title">Work</span>
+                <h3 className="subtitle">What Else I Have Done?</h3>
+                <div className="work_content">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
     );
 }
 
