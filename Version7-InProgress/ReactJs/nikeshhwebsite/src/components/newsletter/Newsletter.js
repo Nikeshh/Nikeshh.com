7d04621c3d94155d1a9a5a0edb1c926f77f5cbf9
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-10 11:29:13 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
@@ -25,15 +25,79 @@ const Newsletter = () => {
                     <h2>Newsletter Downloads</h2>
                     <img src={line} alt="Line" className="newsletter_downloads_line" />
                 </div>
-                <div className="newsletter_downloads_content">
-                    <div className="row">
+                <div className="newsletter_downloads_contents">
+                    <div className="row newsletter_downloads_content">
                         <div className="col-lg-4 content_number">
                             <p>01</p>
                         </div>
                         <div className="col-lg-4 content_metadata">
-                            <h5>Orgaize the Content Moderators</h5>
-                            <h6>Eric Ravensoft in UI / UX Design</h6>
-                            <p>MAR 20 &bull; 5 MIN READ</p>
+                            <h3 style={{ fontWeight: "500" }}>Orgaize the Content Moderators</h3>
+                            <h6 style={{ fontWeight: "300" }}>Eric Ravensoft in UI / UX Design</h6>
+                            <p style={{ fontWeight: "200" }}>MAR 20 &bull; 5 MIN READ</p>
+                        </div>
+                        <div className="col-lg-4 content_actions">
+                            <div>
+                                <button className="preview">Preview</button>
+                                <button className="download">Download</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row newsletter_downloads_content">
+                        <div className="col-lg-4 content_number">
+                            <p>02</p>
+                        </div>
+                        <div className="col-lg-4 content_metadata">
+                            <h3 style={{ fontWeight: "500" }}>Orgaize the Content Moderators</h3>
+                            <h6 style={{ fontWeight: "300" }}>Eric Ravensoft in UI / UX Design</h6>
+                            <p style={{ fontWeight: "200" }}>MAR 20 &bull; 5 MIN READ</p>
+                        </div>
+                        <div className="col-lg-4 content_actions">
+                            <div>
+                                <button className="preview">Preview</button>
+                                <button className="download">Download</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row newsletter_downloads_content">
+                        <div className="col-lg-4 content_number">
+                            <p>03</p>
+                        </div>
+                        <div className="col-lg-4 content_metadata">
+                            <h3 style={{ fontWeight: "500" }}>Orgaize the Content Moderators</h3>
+                            <h6 style={{ fontWeight: "300" }}>Eric Ravensoft in UI / UX Design</h6>
+                            <p style={{ fontWeight: "200" }}>MAR 20 &bull; 5 MIN READ</p>
+                        </div>
+                        <div className="col-lg-4 content_actions">
+                            <div>
+                                <button className="preview">Preview</button>
+                                <button className="download">Download</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row newsletter_downloads_content">
+                        <div className="col-lg-4 content_number">
+                            <p>04</p>
+                        </div>
+                        <div className="col-lg-4 content_metadata">
+                            <h3 style={{ fontWeight: "500" }}>Orgaize the Content Moderators</h3>
+                            <h6 style={{ fontWeight: "300" }}>Eric Ravensoft in UI / UX Design</h6>
+                            <p style={{ fontWeight: "200" }}>MAR 20 &bull; 5 MIN READ</p>
+                        </div>
+                        <div className="col-lg-4 content_actions">
+                            <div>
+                                <button className="preview">Preview</button>
+                                <button className="download">Download</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row newsletter_downloads_content">
+                        <div className="col-lg-4 content_number">
+                            <p>05</p>
+                        </div>
+                        <div className="col-lg-4 content_metadata">
+                            <h3 style={{ fontWeight: "500" }}>Orgaize the Content Moderators</h3>
+                            <h6 style={{ fontWeight: "300" }}>Eric Ravensoft in UI / UX Design</h6>
+                            <p style={{ fontWeight: "200" }}>MAR 20 &bull; 5 MIN READ</p>
                         </div>
                         <div className="col-lg-4 content_actions">
                             <div>
