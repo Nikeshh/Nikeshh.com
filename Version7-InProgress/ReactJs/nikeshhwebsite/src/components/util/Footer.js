467b2ba93153b96306db9f581fd44cbe1742635d
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-10 16:24:29 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/util/Footer.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/util/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
         <div className="footer container">
             <div className="row">
                 <div className="col-md-4">
-                    <div className="footer_logo">
-                        <img src={"/assets/images/logo.svg"} alt="logo" />
-                        <span>Nikeshh V</span>
+                    <div className="footer_logo" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start' }}>
+                        <img src={"/assets/images/loading.gif"} alt="logo" width="100px" />
+                        <span style={{ fontWeight: '700', fontSize: '25px' }}>Nikeshh Vijayabaskaran</span>
                     </div>
-                    <div className="social-icons">
+                    <div className="social-icons" style={{ marginTop: '10px', marginLeft: '100px' }}>
                         <a href="#" className="social-icon"><i className="fa-brands fa-facebook-f"></i></a>
                         <a href="#" className="social-icon"><i className="fa-brands fa-instagram"></i></a>
                         <a href="#" className="social-icon"><i className="fa-brands fa-twitter"></i></a>
@@ -41,11 +41,13 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className="col-md-2">
-                    <button className="btn btn-warning"><span>Info about this website development</span></button>
+                    <button style={{ backgroundColor: '#FF946D', borderRadius: '10px', padding: '5px' }}>
+                        <span>Info about this website development</span>
+                    </button>
                 </div>
             </div>
-            <div style={{ textAlign: "center" }}>
-                <p>Copyright 2022. Made with love by Nikeshh Vijayabaskaran.</p>
+            <div style={{ textAlign: "center", marginTop: '10px' }}>
+                <p style={{ fontStyle: 'italic' }}>Copyright 2022. Made with &#9829; by Nikeshh Vijayabaskaran.</p>
             </div>
         </div>
     );
