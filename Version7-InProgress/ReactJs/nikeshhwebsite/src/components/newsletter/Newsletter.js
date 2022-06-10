b2b6efa8b1e2489e7cbdfcb6fc8294a0df5fee9a
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-10 11:21:55 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/newsletter/Newsletter.js
@@ -1,92 +1,46 @@
 import React from "react";
 import "./newsletter.css";
+import line from "../../images/line.png";
 
 const Newsletter = () => {
     return (
         <div className="newsletter">
-            <span className="title">Newsletter</span>
-            <h3 className="subtitle">I Write On Topics Like Technology, Coding, Product Development Etc</h3>
-            <div className="row">
-                <div className="col-lg-4">
-                    <h3>Newsletter Signup</h3>
-                </div>
-                <div className="col-lg-8">
-                    <div style={{ display: 'flex' }}>
-                        <input type="email" id="email_id" name="email_id" placeholder="Your email id" />
-                        <i className="fa fa-arrow-right"></i>
+            <div className="newsletter_signup">
+                <span className="title">Newsletter</span>
+                <h3 className="subtitle">I Write On Topics Like Technology, Coding, Product Development Etc</h3>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <h3>Newsletter Signup</h3>
+                    </div>
+                    <div className="col-lg-8">
+                        <div style={{ display: 'flex' }}>
+                            <input type="email" id="email_id" name="email_id" placeholder="Your email id" />
+                            <i className="fa fa-arrow-right"></i>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="newsletter_downloads">
-                <h2>Newsletter Downloads</h2>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <p>01</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Orgaize the Content Moderators</h5>
-                        <h6>Eric Ravensoft in UI / UX Design</h6>
-                        <p>MAR 20 . 5 MIN READ</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <button>Preview</button>
-                        <button>Download</button>
-                    </div>
+                <div className="newsletter_downloads_header">
+                    <h2>Newsletter Downloads</h2>
+                    <img src={line} alt="Line" className="newsletter_downloads_line" />
                 </div>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <p>02</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Orgaize the Content Moderators</h5>
-                        <h6>Eric Ravensoft in UI / UX Design</h6>
-                        <p>MAR 20 . 5 MIN READ</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <button>Preview</button>
-                        <button>Download</button>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <p>03</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Orgaize the Content Moderators</h5>
-                        <h6>Eric Ravensoft in UI / UX Design</h6>
-                        <p>MAR 20 . 5 MIN READ</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <button>Preview</button>
-                        <button>Download</button>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <p>04</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Orgaize the Content Moderators</h5>
-                        <h6>Eric Ravensoft in UI / UX Design</h6>
-                        <p>MAR 20 . 5 MIN READ</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <button>Preview</button>
-                        <button>Download</button>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <p>05</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Orgaize the Content Moderators</h5>
-                        <h6>Eric Ravensoft in UI / UX Design</h6>
-                        <p>MAR 20 . 5 MIN READ</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <button>Preview</button>
-                        <button>Download</button>
+                <div className="newsletter_downloads_content">
+                    <div className="row">
+                        <div className="col-lg-4 content_number">
+                            <p>01</p>
+                        </div>
+                        <div className="col-lg-4 content_metadata">
+                            <h5>Orgaize the Content Moderators</h5>
+                            <h6>Eric Ravensoft in UI / UX Design</h6>
+                            <p>MAR 20 &bull; 5 MIN READ</p>
+                        </div>
+                        <div className="col-lg-4 content_actions">
+                            <div>
+                                <button className="preview">Preview</button>
+                                <button className="download">Download</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
