15d90b40d315129aa1015c94a67b44f1c121639d
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-11 15:18:06 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/NotesAndAction.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/NotesAndAction.js
@@ -3,27 +3,21 @@ import "./notesandaction.css";
 
 const NotesAndAction = () => {
     return (
-        <div className="notesandaction">
+        <div className="notesandaction" style={{ marginTop: '100px' }}>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-8">
-                        <p>
-                            I love constructive criticism. I am open to comments and suggestions for my projects.
+                    <div className="col-md-6">
+                        <p style={{ fontFamily: "'Montserrat', sans-serif", fontStyle: 'italic' }}>
+                            I love constructive criticism. I am <b>open to comments and suggestions</b> for my projects.
                             Don’t hesitate to send me a message.
                         </p>
+                        <button className="btn btn-warning" style={{ fontFamily: "'Montserrat', sans-serif", fontWeight: '700', letterSpacing: '5px', wordSpacing: '10px' }}><span>SEND A MESSAGE</span></button>
                     </div>
-                    <div className="col-md-4">
-                        <button className="btn btn-warning"><i className="fa-solid fa-paper-plane"></i><span>Send a message</span></button>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-8">
-                        <p>
-                            I follow a special style of the process to complete my projects in an efficient way
+                    <div className="col-md-6">
+                        <p style={{ fontFamily: "'Montserrat', sans-serif", fontStyle: 'italic' }}>
+                            I follow a <b>special style</b> of the process to complete my projects in an efficient way
                         </p>
-                    </div>
-                    <div className="col-md-4">
-                        <button className="btn btn-warning"><i className="fa-solid fa-paper-plane"></i><span>Check Here</span></button>
+                        <button className="btn btn-warning" style={{ fontFamily: "'Montserrat', sans-serif", fontWeight: '700', letterSpacing: '5px', wordSpacing: '10px' }}><span>CHECK HERE</span></button>
                     </div>
                 </div>
             </div>
