076790677bc91653d40614205f20e021b1931862
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-11 15:50:59 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Mentorship.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Mentorship.js
@@ -7,16 +7,18 @@ const Mentorship = () => {
         <div className="mentorship">
             <span className="title">Mentorship / One-To-One Tutoring</span>
             <h3 className="subtitle">I Like To Help You</h3>
-            <div className="content">
-                <div className="content_with_line">
-                    <h3>Want any help or advice or tutoring?</h3>
-                    <img src={line} alt="Line" className="line" />
+            <div className="content" style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="content_with_line" style={{ position: 'relative' }}>
+                    <h3 style={{ fontFamily: "'Montserrat', sans-serif" }}>Want any help or advice or tutoring?</h3>
+                    <img src={line} alt="Line" style={{ position: 'absolute', left: '30%', top: '80%' }} width="200px" />
                 </div>
-                <h3>I can help you out.</h3>
-                <p>Don't hesistate! Just drop a message and I will get to you as soon as possible.</p>
-                <div className="button_with_line">
+                <h3 style={{ fontFamily: "'Montserrat', sans-serif" }}>I can help you out.</h3>
+                <p style={{ fontFamily: "'Dancing Script', cursive", fontSize: '40px', marginTop: '20px' }}>
+                    Don't hesistate! Just <b style={{ border: '2px dashed black', padding: '10px' }}>drop a message</b> and I will get to you as soon as possible.
+                </p>
+                <div className="button_with_line" style={{ marginTop: '50px', position: 'relative' }}>
                     <button className="btn btn-warning"><span>Start a conversation</span></button>
-                    <img src={line} alt="Line" className="line" />
+                    <img src={line} alt="Line" style={{ position: 'absolute', left: '30%', top: '80%' }} width="300px" />
                 </div>
             </div>
         </div>
