5aae808d1a0a79d8d7af94173c7710e18d11e337
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-12 18:32:01 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ForumScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/ForumScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ForumHeader from "./forum/ForumHeader";
+import Header from "./util/Header";
 import Footer from "./util/Footer";
 import Forum from "./forum/Forum";
 
 const ForumScreen = () => {
     return (
         <>
-            <ForumHeader page_name="forum" />
+            <Header page_name="forum" />
             <Forum />
             <Footer />
         </>
