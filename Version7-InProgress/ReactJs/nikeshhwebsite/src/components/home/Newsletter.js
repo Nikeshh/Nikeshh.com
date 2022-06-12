322bbbf466ef18222035f175f84335bb0a9eb897
Nikeshh <nikeshhbaskaran01@edubay.com.au>
2022-06-12 00:24:06 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Newsletter.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Newsletter.js
@@ -13,11 +13,13 @@ const Newsletter = () => {
                     <button className="btn btn-warning"><span>DOWNLOAD NEWSLETTERS</span></button>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-4">
-                    <h3>Newsletter Signup</h3>
+            <div className="row" style={{ marginTop: '140px' }}>
+                <div className="col-md-4" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <h3 style={{ fontWeight: '300', letterSpacing: '3px' }}>Newsletter Signup</h3>
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-8" style={{ backgroundColor: '#DFD7F9', padding: '50px', borderTopLeftRadius: '50px', borderTopRightRadius: '10px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px', position: 'relative' }}>
+                    <input type="email" id="email_id" name="email_id" placeholder="Your email id" className="form-control"></input>
+                    <i class="fa-solid fa-circle-arrow-right fa-2xl" style={{ position: 'absolute', bottom: '15%', right: '1%' }}></i>
                 </div>
             </div>
         </div>
