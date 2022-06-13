be5466de0d3ad0b89cc0407d079e56583b7a234b
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-13 12:22:38 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Testimonials.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Testimonials.js
@@ -6,6 +6,17 @@ const Testimonials = () => {
         <div className="testimonails">
             <span className="title">Testimonials</span>
             <h3 className="subtitle">See What Others Said About Me</h3>
+            <div className="testimonial-carousel owl-carousel owl-theme">
+                <div className="testimonial-block">
+                    <h1>First Block</h1>
+                </div>
+                <div className="testimonial-block">
+                    <h1>Second Block</h1>
+                </div>
+                <div className="testimonial-block">
+                    <h1>Third Block</h1>
+                </div>
+            </div>
         </div>
     );
 }
