84aad4820bfbf75b5ea084d4497ac4e6ff96f426
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-13 17:33:58 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/blog/BlogCard.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/blog/BlogCard.js
@@ -1,23 +1,65 @@
 import React from "react";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 const BlogCard = () => {
     return (
         <>
             <div className="blog-slider">
-                <div className="blog-slider__wrp swiper-wrapper">
-                    <div className="blog-slider__item swiper-slide">
-                        <div className="blog-slider__img">
-                            <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="" />
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={"auto"}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: false,
+                    }}
+                    pagination={true}
+                >
+                    <div class="blog-slider__wrp swiper-wrapper">
+                        <div class="blog-slider__item swiper-slide">
+                            <div class="blog-slider__img">
+                                <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="" />
+                            </div>
+                            <div class="blog-slider__content">
+                                <span class="blog-slider__code">26 December 2019</span>
+                                <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+                                <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+                                <a href="#" class="blog-slider__button">READ MORE</a>
+                            </div>
                         </div>
-                        <div className="blog-slider__content">
-                            <span class="blog-slider__code">26 December 2019</span>
-                            <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-                            <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
-                            <a href="#" class="blog-slider__button">READ MORE</a>
+                        <div class="blog-slider__item swiper-slide">
+                            <div class="blog-slider__img">
+                                <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/jason-leung-798979-unsplash.webp" alt="" />
+                            </div>
+                            <div class="blog-slider__content">
+                                <span class="blog-slider__code">26 December 2019</span>
+                                <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
+                                <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+                                <a href="#" class="blog-slider__button">READ MORE</a>
+                            </div>
+                        </div>
+                        <div class="blog-slider__item swiper-slide">
+                            <div class="blog-slider__img">
+                                <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/alessandro-capuzzi-799180-unsplash.webp" alt="" />
+                            </div>
+                            <div class="blog-slider__content">
+                                <span class="blog-slider__code">26 December 2019</span>
+                                <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+                                <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+                                <a href="#" class="blog-slider__button">READ MORE</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="blog-slider__pagination"></div>
+                </Swiper>
             </div>
         </>
     );
