135c713545575640f97158a5ec30ef126afbdb9d
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-13 19:56:22 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/skills/Skills.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/skills/Skills.js
@@ -1,11 +1,141 @@
 import React from "react";
-import "./skills.css";
+import './skills.css';
 
 const Skills = () => {
     return (
-        <div className="skills">
-            <span className="title">Support</span>
-            <h3 className="subtitle">Like my work?</h3>
+        <div className="skills container" style={{ marginTop: '40px' }}>
+            <div className="row">
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+                <div className="col-lg-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px' }}>
+                    <img src={"./assets/images/skillsingle.png"} alt="Skill Single" />
+                    <h1 style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700' }}>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Etiam at ultricies dui, eu mattis nisl. Etiam placerat orci 
+                        eu tortor euismod, quis vehicula velit dictum. 
+                        Morbi vehicula, tortor et cursus sollicitudin, massa lectus 
+                        ornare lectus, ut gravida eros velit a libero.
+                    </p>
+                    <div className="ratings">
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star fa-2xl"></i>
+                        <i class="fa-solid fa-star-half-stroke fa-2xl"></i>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '50px' }}>
+                        <button className="btn btn-warning">Read More</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
