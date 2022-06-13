0a327eb7bb9f56a52c48c7f29b6185725fa2cd69
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-13 15:58:49 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/blog/BlogCard.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/blog/BlogCard.js
@@ -4,15 +4,17 @@ const BlogCard = () => {
     return (
         <>
             <div className="blog-slider">
-                <div className="swiper-wrapper">
-                    <div className="swiper-slide">
-                        <h1>First Slide</h1>
-                    </div>
-                    <div className="swiper-slide">
-                        <h1>Second Slide</h1>
-                    </div>
-                    <div className="swiper-slide">
-                        <h1>Third Slide</h1>
+                <div className="blog-slider__wrp swiper-wrapper">
+                    <div className="blog-slider__item swiper-slide">
+                        <div className="blog-slider__img">
+                            <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="" />
+                        </div>
+                        <div className="blog-slider__content">
+                            <span class="blog-slider__code">26 December 2019</span>
+                            <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+                            <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+                            <a href="#" class="blog-slider__button">READ MORE</a>
+                        </div>
                     </div>
                 </div>
                 <div className="blog-slider__pagination"></div>
