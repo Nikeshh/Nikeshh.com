f12b263f03ebe4ff4d3b1b12fa7df5feaccfa287
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-14 16:53:46 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/LoginScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/LoginScreen.js
@@ -1,9 +1,24 @@
 import React from "react";
+import './loginandregister/loginandregister.css';
 
 const LoginScreen = () => {
-    {/* https://codepen.io/hkamci/pen/WNXNqxQ */}
     return (
-        <h1>Login Screen</h1>
+        <div className="wrapper">
+            <div className="left">
+                <div className="sign-in-form active">
+                    <h1>Sign in form</h1>
+                </div>
+                <div className="sign-up-form">
+                    <h1>Sign up form</h1>
+                </div>
+                <div className="forgot-password-form">
+                    <h1>Forgot password form</h1>
+                </div>
+            </div>
+            <div className="right">
+
+            </div>
+        </div>
     );
 }
 
