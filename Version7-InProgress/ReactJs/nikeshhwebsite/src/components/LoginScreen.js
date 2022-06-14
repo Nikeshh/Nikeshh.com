c0fd86600fc09a5c12884914104b1870f0d93f31
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-14 17:09:46 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/LoginScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/LoginScreen.js
@@ -5,18 +5,31 @@ const LoginScreen = () => {
     return (
         <div className="wrapper">
             <div className="left">
-                <div className="sign-in-form active">
+                <div className="sign-in-form">
                     <h1>Sign in form</h1>
                 </div>
                 <div className="sign-up-form">
                     <h1>Sign up form</h1>
                 </div>
-                <div className="forgot-password-form">
-                    <h1>Forgot password form</h1>
+                <div className="forgot-password-form active">
+                    <h1>Forgot Password?</h1>
+                    <form action="">
+                        <div class="form-group">
+                            <label for="">E-mail</label>
+                            <input type="email" placeholder="@mail.com" />
+                        </div>
+                        <div class="form-group">
+                            <button>RESET PASSWORD</button>
+                        </div>
+                        <div class="create-account">
+                            <a href="javascript:;" class="go-to-sign-in">Go Back</a>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div className="right">
-
+                <img src={"/assets/images/loginandregister.svg"} alt="Login and Register" />
+                <p style={{ fontFamily: "'Dancing Script', cursive", fontWeight: '700', letterSpacing: '2px', fontSize: '40px' }}>Maybe you will get more if you have a account!</p>
             </div>
         </div>
     );
