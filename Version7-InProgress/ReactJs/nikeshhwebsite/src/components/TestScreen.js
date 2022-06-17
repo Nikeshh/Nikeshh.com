cb55f8c60177757df7d6e3eadd45a25603f6d51e
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-17 12:23:16 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/TestScreen.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/TestScreen.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Header from "./util/Header";
-import Footer from "./util/Footer";
 import Test from "./test/Test";
 
 const TestScreen = () => {
     return (
         <>
-            <Header page_name="home" />
             <Test />
-            <Footer />
         </>
     );
 }
--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/test/Test.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/test/Test.js
@@ -4,14 +4,6 @@ import './test.css';
 const Test = () => {
     return (
         <>
-            <div className="row test">
-                <div className="col-lg-6">
-                    <img src={"./assets/images/loading.gif"} alt="Landing Image" width="500px" />
-                </div>
-                <div className="col-lg-6">
-                    <img src={"./assets/images/loading.gif"} alt="Landing Image" width="500px" />
-                </div>
-            </div>
         </>
     );
 };
