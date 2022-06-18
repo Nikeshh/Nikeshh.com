14b78c2b8a7e0513c5e89c1b40cca7f10b7b8187
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-17 22:58:34 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/test/Test.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/test/Test.js
@@ -3,8 +3,20 @@ import './test.css';
 
 const Test = () => {
     return (
-        <>
-        </>
+        <div className="test">
+            <nav className="sidebar">
+                <div className="logo-with-text">
+                    <img src={"/assets/images/testlogo.png"} alt="Logo" className="logo" />
+                    <div className="logo-text">
+                        <span className="title">CodingLab</span>
+                        <span className="subtitle">Web Developer</span>
+                    </div>
+                </div>
+            </nav>
+            <div className="home">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
     );
 };
 
