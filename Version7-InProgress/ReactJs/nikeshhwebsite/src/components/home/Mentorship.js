aa63352c52bacb6980ab502f6bfa1c6fa13eb22c
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-20 13:54:04 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Mentorship.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/Mentorship.js
@@ -7,16 +7,16 @@ const Mentorship = () => {
         <div className="mentorship">
             <span className="title">Mentorship / One-To-One Tutoring</span>
             <h3 className="subtitle">I Like To Help You</h3>
-            <div className="content" style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <div className="row content" style={{ marginTop: '50px' }}>
                 <div className="content_with_line" style={{ position: 'relative' }}>
                     <h3 style={{ fontFamily: "'Montserrat', sans-serif" }}>Want any help or advice or tutoring?</h3>
                     <img src={line} alt="Line" style={{ position: 'absolute', left: '30%', top: '80%' }} width="200px" />
                 </div>
                 <h3 style={{ fontFamily: "'Montserrat', sans-serif" }}>I can help you out.</h3>
                 <p style={{ fontFamily: "'Dancing Script', cursive", fontSize: '40px', marginTop: '20px' }}>
-                    Don't hesistate! Just <b style={{ border: '2px dashed black', padding: '10px' }}>drop a message</b> and I will get to you as soon as possible.
+                    Don't hesistate! Just <div className="drop_a_message"><b style={{ border: '2px dashed black', padding: '10px' }}>drop a message</b></div> and I will get to you as soon as possible.
                 </p>
-                <div className="button_with_line" style={{ marginTop: '50px', position: 'relative' }}>
+                <div className="button_with_line" style={{ marginTop: '50px', position: 'relative', display: 'flex', justifyContent: 'center' }}>
                     <button className="btn btn-warning"><span>Start a conversation</span></button>
                 </div>
             </div>
