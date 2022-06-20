6f03dea3092d3d760f6afd4fa730d1f8c19d970e
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-06-20 13:49:14 -0400

    Website changes

--- Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/NotesAndAction.js
+++ Version7-InProgress/ReactJs/nikeshhwebsite/src/components/home/NotesAndAction.js
@@ -6,14 +6,14 @@ const NotesAndAction = () => {
         <div className="notesandaction" style={{ marginTop: '100px' }}>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="notes col-xs-12 col-md-6">
                         <p style={{ fontFamily: "'Montserrat', sans-serif", fontStyle: 'italic' }}>
                             I love constructive criticism. I am <b>open to comments and suggestions</b> for my projects.
                             Don’t hesitate to send me a message.
                         </p>
                         <button className="btn btn-warning" style={{ fontFamily: "'Montserrat', sans-serif", fontWeight: '700', letterSpacing: '5px', wordSpacing: '10px' }}><span>SEND A MESSAGE</span></button>
                     </div>
-                    <div className="col-md-6">
+                    <div className="notes col-xs-12 col-md-6">
                         <p style={{ fontFamily: "'Montserrat', sans-serif", fontStyle: 'italic' }}>
                             I follow a <b>special style</b> of the process to complete my projects in an efficient way
                         </p>
