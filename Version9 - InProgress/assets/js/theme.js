9c35d66ce5904a35c747b538ca50fc7dc0a220af
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-12-09 10:56:19 -0500

    Website changes

---
+++ Version9 - InProgress/assets/js/theme.js
@@ -0,0 +1,56 @@
+/* Used to switch the theme using a toggle button */
+// const toggleButton = document.querySelector('.toggle-button');
+// toggleButton.addEventListener('change', toggleTheme, false);
+
+window.onload = function() {
+  useTheme('dark');
+};
+
+const theme = {
+  dark: {
+    '--primary-background-color': '#000013',
+    '--primary-color': '#325b97',
+    '--secondary-color': '#9cafeb',
+    '--font-color': '#e1e1ff',
+    '--bg-color': '#000013',
+    '--heading-color': '#818cab'
+  },
+  light: {
+    '--primary-background-color': '#ffffff',
+    '--primary-color': '#0d0b52',
+    '--secondary-color': '#3458b9',
+    '--font-color': '#424242',
+    '--bg-color': '#ffffff',
+    '--heading-color': '#292922'
+  }
+};
+
+function toggleTheme(e) {
+  if (e.target.checked) {
+    useTheme('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    useTheme('light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+function useTheme(themeChoice) {
+  document.documentElement.style.setProperty('--primary-background-color', theme[themeChoice]['--primary-background-color']);
+  document.documentElement.style.setProperty('--primary-color', theme[themeChoice]['--primary-color']);
+  document.documentElement.style.setProperty('--secondary-color',theme[themeChoice]['--secondary-color']);
+  document.documentElement.style.setProperty('--font-color', theme[themeChoice]['--font-color']);
+  document.documentElement.style.setProperty('--bg-color',theme[themeChoice]['--bg-color']);
+  document.documentElement.style.setProperty('--heading-color',theme[themeChoice]['--heading-color']);
+}
+
+const preferredTheme = localStorage.getItem('theme');
+if (preferredTheme === 'dark') {
+  useTheme('dark');
+  /* Used to switch the theme using a toggle button */
+  // toggleButton.checked = true;
+} else {
+  useTheme('light');
+  /* Used to switch the theme using a toggle button */
+  // toggleButton.checked = false;
+}
