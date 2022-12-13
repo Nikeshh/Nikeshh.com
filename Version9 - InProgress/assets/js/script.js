e7bbaec5c253706e80e3ac0f13d237cde3400975
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-12-12 21:08:29 -0500

    Website changes

--- Version9 - InProgress/assets/js/script.js
+++ Version9 - InProgress/assets/js/script.js
@@ -1,15 +1,15 @@
-const dropdown = document.querySelector('.dropdown');
+/*const dropdown = document.querySelector('.nav-item-dropdown');
 dropdown.addEventListener('mouseover', displayDropdown, false);
 dropdown.addEventListener('mouseout', closeDropdown, false);
 
 function displayDropdown() {
-    const navItemDropdown = document.querySelector('.nav-item-dropdown');
+    const navItemDropdown = document.querySelector('.dropdown-items');
     navItemDropdown.style.display = 'block';
     navItemDropdown.style.opacity = '100';
 }
 
 function closeDropdown() {
-    const navItemDropdown = document.querySelector('.nav-item-dropdown');
+    const navItemDropdown = document.querySelector('.dropdown-items');
     navItemDropdown.style.display = 'none';
     navItemDropdown.style.opacity = '0';
-}
+}*/
