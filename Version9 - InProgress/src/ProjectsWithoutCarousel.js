ee9a4a9c10bf333bc16cee2ea418a79a28a245b1
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2023-02-27 23:48:20 -0500

    Website changes

--- Version9 - InProgress/src/ProjectsWithoutCarousel.js
+++ Version9 - InProgress/src/ProjectsWithoutCarousel.js
@@ -1,5 +1,5 @@
 import Project from './projects/Project.js';
-import { projects } from './projects/data.js';
+import { projects } from './data/data.js';
 
 var ProjectsWithoutCarousel = function ProjectsWithoutCarousel() {
     return React.createElement(
