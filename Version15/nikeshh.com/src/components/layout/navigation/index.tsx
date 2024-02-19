6dbbbf8634537aacb80fb269f0b28cdacd591a78
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-19 18:18:38 -0500

    Updated navigation and notification

--- Version15/nikeshh.com/src/components/layout/navigation/index.tsx
+++ Version15/nikeshh.com/src/components/layout/navigation/index.tsx
@@ -27,8 +27,8 @@ const Navigation = () => {
             <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
                 <ul className="flex items-center justify-center gap-8">
                     <Link href={'#'} className="hover:text-blue-600">Home</Link>
-                    <Link href={'#'} className="hover:text-blue-600">Services</Link>
-                    <Link href={'#'} className="hover:text-blue-600">Blog</Link>
+                    <Link href={'#'} className="hover:text-blue-600">Skills</Link>
+                    <Link href={'#'} className="hover:text-blue-600">Projects</Link>
                 </ul>
             </nav>
             <aside className="flex gap-2 items-center">
--- Version15/nikeshh.com/src/components/layout/notification/index.tsx
+++ Version15/nikeshh.com/src/components/layout/notification/index.tsx
@@ -1,6 +1,6 @@
 const NotificationC = () => {
     return (
-        <div className="bg-indigo-900 text-center py-4 px-6 lg:px-4">
+        <div className="bg-indigo-900 text-center py-4 px-6 lg:px-4 cursor-pointer">
             <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none md:rounded lg:rounded-full flex lg:inline-flex" role="alert">
                 <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">SAAS Alert</span>
                 <span className="font-semibold mr-2 text-left flex-auto">Website builder for agencies</span>
