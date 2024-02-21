61b9c269aba5947f9659a2ccdb8aaa584e78caa0
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-20 22:22:49 -0500

    Services section changes

--- Version15/nikeshh.com/src/app/home/_components/services/index.tsx
+++ Version15/nikeshh.com/src/app/home/_components/services/index.tsx
@@ -51,85 +51,87 @@ const Services = ({ services } : Props) => {
     }
 
     if (services && services.length > 0 && serviceTags) {
-        <section id="services" className="container pt-12 md:pt-44 relative flex flex-col items-center justify-center">
-            <p>BUILT FOR SCALING & VALUE</p>
-            <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-                <h2 className="font-bold text-xl md:text-[40px] md:leading-none text-center">
-                    SERVICES
-                </h2>
-            </div>
-            <div className="mt-6 flex flex-wrap gap-2 justify-center">
-                {serviceTags.map((a) => {
-                    return (
-                        <Badge variant={selectedService == a ? 'default' : 'outline'} key={a}>
-                            <div
-                                className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end cursor-pointer"
-                                onClick={() => updateSelectedService(a)}
-                            >
-                                <span className={selectedService == a ? 'text-white transition' : 'text-gray-700 transition group-hover:text-gray-700/75'}>
-                                    {a}
-                                </span>
-                            </div>
-                        </Badge>
-                    );
-                })}
-            </div>
-            <div className="flex flex-wrap md:flex-nowrap justify-center gap-4 pt-9">
-                <Command>
-                    <CommandInput placeholder="Type a command or search..." />
-                    <CommandList>
-                        <CommandEmpty>No results found.</CommandEmpty>
-                        {Object.keys(selectedServices).map((service) => {
-                            const subServices = selectedServices[service];
-                            return (
-                            <div key={service}>
-                                <CommandGroup heading={service}>
-                                    {subServices.map((subService: ServicesType) => {
-                                        return (
-                                            <CommandItem key={subService.id} onSelect={() => setServiceCommand(subService)} className="cursor-pointer">{subService.name}</CommandItem>
-                                        );
-                                    })}
-                                </CommandGroup>
-                                <CommandSeparator/>
-                            </div>
-                            );
-                        })}
-                    </CommandList>
-                </Command>
-                <Card className="min-w-[200px] max-w-[450px]">
-                    <CardHeader>
-                        <CardTitle>{serviceCommand.name}</CardTitle>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <p>{serviceCommand.subtitle}</p>
-                    </CardContent>
-                    <CardFooter>
-                        <div className="hover:text-blue-600 underline cursor-pointer" onClick={() => {
-                            setOpen(
-                            <CustomModal
-                                title="⚠️ Under Construction"
-                                subheading="Should be available in 2-3 days"
-                            >
-                                <p>You can track the progress or contribute @<Link href="https://github.com/Nikeshh/Nikeshh.com/tree/main/Version15/nikeshh.com" className="underline cursor-pointer">Github</Link></p>
-                            </CustomModal>
-                            )
-                        }}>explore more.</div>
-                    </CardFooter>
-                </Card>
-            </div>
-            <div className='mt-4 md:mt-6 w-full text-center'>
-            <div className="hover:text-blue-600 underline cursor-pointer" onClick={() => {
-                setOpen(
-                    <CustomModal
-                        title="⚠️ Under Construction"
-                        subheading="Should be available in 2-3 days"
-                    >
-                        <p>You can track the progress or contribute @<Link href="https://github.com/Nikeshh/Nikeshh.com/tree/main/Version15/nikeshh.com" className="underline cursor-pointer">Github</Link></p>
-                    </CustomModal>
-                )
-            }}>view more.</div>
-            </div>
-        </section>
+        return (
+            <section id="services" className="container pt-12 md:pt-44 relative flex flex-col items-center justify-center">
+                <p>BUILT FOR SCALING & VALUE</p>
+                <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+                    <h2 className="font-bold text-xl md:text-[40px] md:leading-none text-center">
+                        SERVICES
+                    </h2>
+                </div>
+                <div className="mt-6 flex flex-wrap gap-2 justify-center">
+                    {serviceTags.map((a) => {
+                        return (
+                            <Badge variant={selectedService == a ? 'default' : 'outline'} key={a}>
+                                <div
+                                    className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end cursor-pointer"
+                                    onClick={() => updateSelectedService(a)}
+                                >
+                                    <span className={selectedService == a ? 'text-white transition' : 'text-gray-700 transition group-hover:text-gray-700/75'}>
+                                        {a}
+                                    </span>
+                                </div>
+                            </Badge>
+                        );
+                    })}
+                </div>
+                <div className="flex flex-wrap md:flex-nowrap justify-center gap-4 pt-9">
+                    <Command>
+                        <CommandInput placeholder="Type a command or search..." />
+                        <CommandList>
+                            <CommandEmpty>No results found.</CommandEmpty>
+                            {Object.keys(selectedServices).map((service) => {
+                                const subServices = selectedServices[service];
+                                return (
+                                <div key={service}>
+                                    <CommandGroup heading={service}>
+                                        {subServices.map((subService: ServicesType) => {
+                                            return (
+                                                <CommandItem key={subService.id} onSelect={() => setServiceCommand(subService)} className="cursor-pointer">{subService.name}</CommandItem>
+                                            );
+                                        })}
+                                    </CommandGroup>
+                                    <CommandSeparator/>
+                                </div>
+                                );
+                            })}
+                        </CommandList>
+                    </Command>
+                    <Card className="min-w-[200px] max-w-[450px]">
+                        <CardHeader>
+                            <CardTitle>{serviceCommand.name}</CardTitle>
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            <p>{serviceCommand.subtitle}</p>
+                        </CardContent>
+                        <CardFooter>
+                            <div className="hover:text-blue-600 underline cursor-pointer" onClick={() => {
+                                setOpen(
+                                <CustomModal
+                                    title="⚠️ Under Construction"
+                                    subheading="Should be available in 2-3 days"
+                                >
+                                    <p>You can track the progress or contribute @<Link href="https://github.com/Nikeshh/Nikeshh.com/tree/main/Version15/nikeshh.com" className="underline cursor-pointer">Github</Link></p>
+                                </CustomModal>
+                                )
+                            }}>explore more.</div>
+                        </CardFooter>
+                    </Card>
+                </div>
+                <div className='mt-4 md:mt-6 w-full text-center'>
+                <div className="hover:text-blue-600 underline cursor-pointer" onClick={() => {
+                    setOpen(
+                        <CustomModal
+                            title="⚠️ Under Construction"
+                            subheading="Should be available in 2-3 days"
+                        >
+                            <p>You can track the progress or contribute @<Link href="https://github.com/Nikeshh/Nikeshh.com/tree/main/Version15/nikeshh.com" className="underline cursor-pointer">Github</Link></p>
+                        </CustomModal>
+                    )
+                }}>view more.</div>
+                </div>
+            </section>
+        );
     } else {
         return <></>;
     }
