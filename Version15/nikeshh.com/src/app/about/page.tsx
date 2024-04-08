e5d9ccf93b159c3e21f21a029a4e82e2cb16d83e
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-08 15:25:49 -0400

    About page changes

--- Version15/nikeshh.com/src/app/about/page.tsx
+++ Version15/nikeshh.com/src/app/about/page.tsx
@@ -6,19 +6,18 @@ import { components } from "@/slices";
 import Bounded from "@/components/Bounded";
 import StarGrid from "@/components/StarGrid";
 import { PrismicNextImage } from "@prismicio/next";
-import { asText } from "@prismicio/client";
 import Navigation from "@/components/layout/navigation";
 import NotificationC from "@/components/layout/notification";
 import Footer from "@/components/layout/footer";
 
 type Params = { uid: string };
 
-export default async function Page({ params }: { params: Params }) {
+const Page = async () => {
     console.log("👋 Hi! It looks like you are trying to explore the code. You can directly work with me on any of the listed projects. Feel free to submit your details through the contact form or directly mail me at nikeshhbaskaran01@gmail.com. Cheers 🍻");
 
     const client = createClient();
     const page = await client
-        .getByUID("about", params.uid)
+        .getByUID("about", "about")
         .catch(() => notFound());
 
     return (
@@ -30,7 +29,7 @@ export default async function Page({ params }: { params: Params }) {
                   <StarGrid />
                   <h1 className="text-7xl font-medium">
                       <PrismicText field={page.data.company} />
-                      <p className="text-lg text-yellow-500">Case Study</p>
+                      <p className="text-lg text-yellow-500">About Me</p>
                   </h1>
                   <p className="mb-4 mt-8 max-w-xl text-lg text-slate-300">
                       <PrismicText field={page.data.description} />
@@ -42,7 +41,7 @@ export default async function Page({ params }: { params: Params }) {
                   />
               </div>
               <div className="mx-auto">
-                  <SliceZone slices={page.data.slices} components={components} />
+                <SliceZone slices={page.data.slices} components={components} />
               </div>
           </Bounded>
           <Footer/>
@@ -50,27 +49,4 @@ export default async function Page({ params }: { params: Params }) {
     );
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
-  const client = createClient();
-  const page = await client
-    .getByUID("about", params.uid)
-    .catch(() => notFound());
-
-  return {
-    title: `${page.data.meta_title || asText(page.data.company) + " Case Study"}`,
-    description: page.data.meta_description,
-  };
-}
-
-export async function generateStaticParams() {
-  const client = createClient();
-  const pages = await client.getAllByType("about");
-
-  return pages.map((page) => {
-    return { uid: page.uid };
-  });
-}
+export default Page;
