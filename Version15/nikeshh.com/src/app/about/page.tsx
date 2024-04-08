a8708ea1e06813e1d92a1fd2d37fd4742a50907b
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-08 14:53:57 -0400

    About page and Slice Simulator changes

--- Version15/nikeshh.com/src/app/about/page.tsx
+++ Version15/nikeshh.com/src/app/about/page.tsx
@@ -7,37 +7,47 @@ import Bounded from "@/components/Bounded";
 import StarGrid from "@/components/StarGrid";
 import { PrismicNextImage } from "@prismicio/next";
 import { asText } from "@prismicio/client";
+import Navigation from "@/components/layout/navigation";
+import NotificationC from "@/components/layout/notification";
+import Footer from "@/components/layout/footer";
 
 type Params = { uid: string };
 
 export default async function Page({ params }: { params: Params }) {
-  const client = createClient();
-  const page = await client
-    .getByUID("about", params.uid)
-    .catch(() => notFound());
+    console.log("👋 Hi! It looks like you are trying to explore the code. You can directly work with me on any of the listed projects. Feel free to submit your details through the contact form or directly mail me at nikeshhbaskaran01@gmail.com. Cheers 🍻");
 
-  return (
-    <Bounded as="article">
-      <div className="relative grid place-items-center text-center">
-        <StarGrid />
-        <h1 className="text-7xl font-medium">
-          <PrismicText field={page.data.company} />
-          <p className="text-lg text-yellow-500">Case Study</p>
-        </h1>
-        <p className="mb-4 mt-8 max-w-xl text-lg text-slate-300">
-          <PrismicText field={page.data.description} />
-        </p>
-        <PrismicNextImage
-          field={page.data.logo_image}
-          className="rounded-lg"
-          quality={100}
-        />
-      </div>
-      <div className="mx-auto">
-        <SliceZone slices={page.data.slices} components={components} />
-      </div>
-    </Bounded>
-  );
+    const client = createClient();
+    const page = await client
+        .getByUID("about", params.uid)
+        .catch(() => notFound());
+
+    return (
+        <>
+            <NotificationC />
+            <Navigation />
+            <Bounded as="article">
+                <div className="relative grid place-items-center text-center">
+                    <StarGrid />
+                    <h1 className="text-7xl font-medium">
+                        <PrismicText field={page.data.company} />
+                        <p className="text-lg text-yellow-500">Case Study</p>
+                    </h1>
+                    <p className="mb-4 mt-8 max-w-xl text-lg text-slate-300">
+                        <PrismicText field={page.data.description} />
+                    </p>
+                    <PrismicNextImage
+                        field={page.data.logo_image}
+                        className="rounded-lg"
+                        quality={100}
+                    />
+                </div>
+                <div className="mx-auto">
+                    <SliceZone slices={page.data.slices} components={components} />
+                </div>
+            </Bounded>
+            <Footer/>
+        </>
+    );
 }
 
 export async function generateMetadata({
