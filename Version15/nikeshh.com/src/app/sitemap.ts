c242e34045ff6501d182ed3ceaa12432131db970
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-14 11:22:40 -0400

    Sitemap changes

--- Version15/nikeshh.com/src/app/sitemap.ts
+++ Version15/nikeshh.com/src/app/sitemap.ts
@@ -3,16 +3,22 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://nikeshh.com',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+        url: 'https://nikeshh.com',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
     },
     {
-      url: 'https://nikeshh.com/home',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
+        url: 'https://nikeshh.com/home',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+    },
+    {
+        url: 'https://nikeshh.com/about',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
     }
   ]
 }
