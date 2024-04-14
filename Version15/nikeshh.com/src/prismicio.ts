2111b60dc1f77dbd1b537d3e3ebde4098d004b46
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-14 11:21:04 -0400

    Sitemap and Prismico changes

--- Version15/nikeshh.com/src/prismicio.ts
+++ Version15/nikeshh.com/src/prismicio.ts
@@ -15,15 +15,11 @@ export const repositoryName =
  */
 // TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig["routes"] = [
-  // Examples:
-  // {
-  // 	type: "homepage",
-  // 	path: "/",
-  // },
-  // {
-  // 	type: "page",
-  // 	path: "/:uid",
-  // },
+  {
+    type: "about",
+    uid: "about",
+    path: "/about",
+  },
 ];
 
 /**
