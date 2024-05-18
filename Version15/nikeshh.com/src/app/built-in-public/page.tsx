a8e13febcbd6c7f9eec5e4caff3175e6a27cf3b0
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-05-18 10:23:34 -0400

    Changed banner

--- Version15/nikeshh.com/src/app/built-in-public/page.tsx
+++ Version15/nikeshh.com/src/app/built-in-public/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
               <div className="aspect-w-16 aspect-h-12 overflow-hidden bg-gray-100 rounded-2xl dark:bg-neutral-800">
                 <img
                   className="group-hover:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl"
-                  src="https://images.unsplash.com/photo-1575052814086-f385e2e2ad1b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="/nikeshhcodes-thumbnail.jpg"
                   alt="Image Description"
                 />
               </div>
