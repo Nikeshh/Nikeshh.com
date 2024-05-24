be574ad33292d0a229e59cf6b899a1889d575806
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-05-24 12:35:58 -0400

    removed href from blog title

--- Version15/nikeshh.com/src/app/_components/blogs/index.tsx
+++ Version15/nikeshh.com/src/app/_components/blogs/index.tsx
@@ -60,11 +60,9 @@ const Blogs = ({ blogs }: Props) => {
                     10th Oct 2023
                   </time>
 
-                  <a href="#">
-                    <h3 className="mt-0.5 text-lg text-gray-900 dark:text-white">
-                      {blog.title}
-                    </h3>
-                  </a>
+                  <h3 className="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    {blog.title}
+                  </h3>
 
                   <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 dark:text-gray-400">
                     {blog.subtitle}
