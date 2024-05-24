ff7ec1b1d881ba977b4f3a2d18481774e183fa65
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-05-24 18:12:28 -0400

    Middleware changes

--- Version15/nikeshh.com/src/middleware.ts
+++ Version15/nikeshh.com/src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from "next/server";
  
 const isProtectedRoute = createRouteMatcher([
-  '/store'
+  '/store/cart'
 ]);
 
 export default clerkMiddleware((auth, req) => {
