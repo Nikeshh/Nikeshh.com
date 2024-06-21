959edd2fdc3a1bb736d900c737004dd6da437322
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-06-20 22:19:09 -0400

    Corrected hydration issue

---
+++ Version15/nikeshh.com/src/app/portfolio/main.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useState } from 'react';
+import About from './_components/about';
+import Blog from './_components/blog';
+import Navbar from './_components/navbar';
+import Portfolio from './_components/portfolio';
+import Resume from './_components/resume';
+import Sidebar from './_components/sidebar';
+import Contact from './_components/contact';
+import './style.css';
+
+type Props = {
+    testimonials: {
+        id: string;
+        name: string;
+        content: string;
+        avatarUrl: string;
+        designation: string;
+        companyLogoUrl: string;
+    }[]
+}
+
+const Main = ({ testimonials } : Props) => {
+
+    const [activeNavbar, setActiveNavbar] = useState("About");
+    const [activeSidebar, setActiveSidebar] = useState(false);
+
+    return (
+        <main className='mt-0'>
+            <Sidebar activeSidebar={activeSidebar} setActiveSidebar={setActiveSidebar} />
+            <div className="main-content">
+                <Navbar activeNavbar={activeNavbar} setActiveNavbar={setActiveNavbar} />
+                <About activeNavbar={activeNavbar} testimonials={testimonials} />
+                <Resume activeNavbar={activeNavbar} />
+                <Portfolio activeNavbar={activeNavbar} />
+                <Blog activeNavbar={activeNavbar} />
+                <Contact activeNavbar={activeNavbar} />
+            </div>
+        </main>
+    );
+}
+
+export default Main;
--- Version15/nikeshh.com/src/app/portfolio/page.tsx
+++ Version15/nikeshh.com/src/app/portfolio/page.tsx
@@ -1,36 +1,14 @@
-"use client"
-
-import { useState } from 'react';
-import About from './_components/about';
-import Blog from './_components/blog';
-import Navbar from './_components/navbar';
-import Portfolio from './_components/portfolio';
-import Resume from './_components/resume';
-import Sidebar from './_components/sidebar';
 import './style.css';
-import Contact from './_components/contact';
-import { getBlogs, getProjects, getSkills, getTestimonials } from '@/lib/queries';
+import { getTestimonials } from '@/lib/queries';
+import Main from './main';
 
 const Page = () => {
-
-    const [activeNavbar, setActiveNavbar] = useState("About");
-    const [activeSidebar, setActiveSidebar] = useState(false);
 
     const testimonials = getTestimonials();
 
     return (
-        <main className='mt-0'>
-            <Sidebar activeSidebar={activeSidebar} setActiveSidebar={setActiveSidebar} />
-            <div className="main-content">
-                <Navbar activeNavbar={activeNavbar} setActiveNavbar={setActiveNavbar} />
-                <About activeNavbar={activeNavbar} testimonials={testimonials} />
-                <Resume activeNavbar={activeNavbar} />
-                <Portfolio activeNavbar={activeNavbar} />
-                <Blog activeNavbar={activeNavbar} />
-                <Contact activeNavbar={activeNavbar} />
-            </div>
-        </main>
-    );
+        <Main testimonials={testimonials} />
+    )
 }
 
 export default Page;
