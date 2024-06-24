6d29a413a57eae11b07eb3def33c0a5b4ebe28c8
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-06-23 20:31:16 -0400

    Added target blank

--- Version15/nikeshh.com/src/lib/queries.ts
+++ Version15/nikeshh.com/src/lib/queries.ts
@@ -1779,7 +1779,7 @@ const businessPrespectiveProjects = [
           </tbody>
       </table>
       <div className="flex justify-center">
-        <a href="https://github.com/Nikeshh/VectorChat/" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">View Source Code</a>
+        <a href="https://github.com/Nikeshh/VectorChat/" target="_blank" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">View Source Code</a>
       </div>
     </div>
     `,
